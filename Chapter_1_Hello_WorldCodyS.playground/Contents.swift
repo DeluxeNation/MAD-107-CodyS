bad0550877105374b10664a60cb7894764287a01
DeluxeNation <91623968+DeluxeNation@users.noreply.github.com>
2023-01-31 14:09:01 -0600

    First Commit

---
+++ Chapter_1_Hello_WorldCodyS.playground/Contents.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+let name = "Cody",language = "Swift"
+var message1 = " Welcome to the wonderful world of "
+var message2 = "\(name), Welcome to the wonderful world of \(language)!"
+print(message2)
+print(name, message1, language,separator: "",terminator:  "!")
